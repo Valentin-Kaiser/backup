fb48f44880d3d40aad7b5c99fdafb7c019b840a4
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2025-01-08 10:47:32 +0100

    [FIX] bumped restic version

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
 DATE=$(date +"%Y.%m.%d %H:%M:%S")
-echo "$DATE: => Starting backup" | tee -a /var/log/backup.log
-
+echo "$DATE: => Starting backup - $RESTIC_SOURCE" | tee -a /var/log/backup.log
 restic backup $RESTIC_BACKUP_ARGS "$RESTIC_SOURCE" | tee -a /var/log/backup.log 2>&1
+echo "$DATE: => Backup finished" | tee -a /var/log/backup.log
